1e8ceb6773be4d722abf06b171d859af095ec1c9
deoiveij <deoiveij@uni.coventry.ac.uk>
2018-11-18 22:43:58 +0000

    fix file from graph, end4today start study Dijkstra Algorithm and graphs

--- 1 Graph/graph.cpp
+++ 1 Graph/graph.cpp
@@ -30,64 +30,14 @@ class graph{
 	bool addVert(){
 
 	}
+
+
+
+
 };
 
-string checkInput(auto& path2)
-{
-
-	/*Accept input and separate unique numbers and create a vector for them(vertices)
-	for edges check on many times the vertice colapsa com outros */
-
-	vector<string> pathFix;
-	int counter=0;
-
-	istringstream iss(path2);
-	    copy(istream_iterator<string>(iss),istream_iterator<string>(),back_inserter(pathFix));
-
-	vector<string> pathNum={};
-	for(int x=0; x <= pathFix.size();x++)
-	{
-		cout<<pathFix[x]<<endl;
-
-	}
 
 
-
-
-/*
-	for(int x=0; x <= 5;x++)
-	{
-		if(find(path2G.begin(), path2G.end(), possiblePath[x]) != path2G.end())
-		{
-			counter=1;
-			return possiblePath[x].c_str();
-		}
-
-	}
-	if(counter==0){
-		return string("false");
-	}*/
-}
-
-void addTXT(int text,string desc){
-
-	fstream file;
-	  file.open ("log.txt",ios::app);
-
-
-	  // Writing on file
-	  file << text << endl;
-	  file <<"Desc: "<<desc << endl;
-
-
-	  file.close();
-}
-
-bool splitInput(auto% numberTxt){
-
-
-
-}
 
 int main(){
 	int numPath;
@@ -105,31 +55,16 @@ int main(){
 	}
 		cin.clear(); 
 		cin.ignore(INT_MAX, '\n');
-	for (int x=0; x<=numPath-1;x++){
+	while(){
 
 
 		cout<<string(10,' ')<<"Insert the "<<x+1<<" path"<<endl;
 		cin.getline(number_VE, sizeof(number_VE));
 
-		while(splitInput()==false){
-			
-
-			cout<<RED_TEXT("You can only input a two single number.")<<endl;
-			cout<<RED_TEXT("Use Space beetwen the numbers e.g -> 2 3")<<endl;
-			cout<<string(10,' ')<<"Insert the "<<x+1<<" path"<<endl;
-			cin.getline(number_VE, sizeof(number_VE));
-
-
-
-		}
-		//vert_edges.emplace_back(number_VE);
-
 	}
-		for (int x=0; x<=vert_edges.size();x++){
 
 
-				cout<<string(10,' ')<<vert_edges[x]<<endl;
 
-			}
+
 	return 0;
 }
